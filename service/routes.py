2ca98748df57347e4bea9ed557bb288dbf70d4fa
ChizhiWang <920173862@qq.com>
2023-10-17 15:10:10 -0400

    add product.quantity to db model

--- service/routes.py
+++ service/routes.py
@@ -125,19 +125,20 @@ def create_products(wishlist_id):
     message = new_product.serialize()
     return make_response(jsonify(message), status.HTTP_201_CREATED)
 
+
 ######################################################################
 # RETRIEVE a product in the wishlist
 ######################################################################
-@app.route(
-    f"{BASE_URL}/<int:wishlist_id>/products/<int:product_id>", methods=["GET"]
-)
+@app.route(f"{BASE_URL}/<int:wishlist_id>/products/<int:product_id>", methods=["GET"])
 def get_product(wishlist_id, product_id):
     """
     Get an product
 
     This endpoint returns just an product
     """
-    app.logger.info("Request to retrieve product %s in wishilist id: %s", (product_id, wishlist_id))
+    app.logger.info(
+        "Request to retrieve product %s in wishilist id: %s", (product_id, wishlist_id)
+    )
 
     # See if the product exists and abort if it doesn't
     product = Product.find(product_id)
