0c1e6fb1be8943258528afd8d13ae08b54b1c1c7
ChizhiWang <920173862@qq.com>
2023-11-28 19:28:12 -0500

    modify wishlist_steps.py

--- features/steps/wishlists_steps.py
+++ features/steps/wishlists_steps.py
@@ -40,11 +40,12 @@ def step_impl(context):
         expect(context.resp.status_code).to_equal(204)
 
     # load the database with new wishlists
+    current_date = datetime.now().date()
     for row in context.table:
         payload = {
             "name": row["name"], 
             "owner": row["user_name"],
-            "date_joined": datetime.now().date(),
+            "date_joined": current_date,
             "products": [],
         }
         context.resp = requests.post(rest_endpoint, json=payload)
